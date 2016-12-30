cb099fca83054abbf4daa5457ffa264757382c3b
Cresse Kenneth Abenojar <alakneth@gmail.com>
2016-12-30 22:33:30 +0800

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,117 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>Concho's Sisig</title>
+		<link rel="icon" href="img/logo.png">
+
+		<!-- CUSTOM JS/CSS -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+		<!-- FLAT UI JS/CSS -->
+		<link rel="stylesheet" type="text/css" href="Ui/dist/css/flat-ui.css">
+		<link rel="stylesheet" type="text/css" href="Ui/dist/css/flat-ui.min.css">
+		<link rel="stylesheet" type="text/css" href="Ui/dist/css/flat-ui.css.map">
+		<link rel="stylesheet" type="text/css" href="Ui/dist/css/vendor/bootstrap/css/bootstrap.min.css">
+		<script type="text/javascript" src="Ui/dist/js/flat-ui.js"></script>
+
+		<!-- Font awesome -->
+		<link rel="stylesheet" type="text/css" href="fonts/css/font-awesome.css">
+
+
+		<!-- Animate -->
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+		<!-- JQUERY -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/default.js"></script>
+	</head>
+
+	<body>
+
+		<!-- Start of -->
+		<!-- End of -->
+
+
+
+		<!-- Start of Container -->
+		<div class="container">
+				<!-- Start of header-->
+				 <div class="header">
+					<ul class="left">
+						<li class="active logo"><b><a href="#home"><i class="fa fa-shopping-cart"></i> djfh</a></b></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#gallery">Gallery</a></li>
+						<li><a href="#contact">Contact</a></li>
+						<li><a href="#signup">Facebook</a></li>
+					</ul>
+
+
+					<ul class="right">
+						<li><a href="#about">SignUp</a></li>
+						<li><a href="#about">Terms</a></li>
+						<li><a href="#contact"><i class="fa fa-user"></i></a></li>
+						<li><a href="#signup"><i class="fa fa-search"></i></a></li>
+					</ul>
+
+				</div>
+				<!-- ENd of header-->
+			<!-- Start of Video Bg-->
+		 	<div class="video-bg">
+
+		 		<div class="welcome">
+		 			<center>
+		 				<h1>Concho's Sisig<br>SHOP & EAT</h1>
+		 				<p>Our specialty (obviously). Grab your go-to or try something new, you wont regret it. These hearty triangles arent for the weak-hearted. Build it the way you desire, we make it fresh, you eat, you win. </p>
+
+		 				<div class="buttons">
+		 					<button class="blue">EAT</button>
+			 				<button class="pink">SLEEP</button>
+			 				<button class="gray">SHOP</button>
+		 				</div>
+		 			</center>
+		 		</div>
+
+		 		<video autoplay="" name="media" loop muted class="video">
+		 			<source src="img/bg.mp4" type="video/mp4">
+		 		</video>
+
+		 	</div>
+			<!-- End of Video Bg -->
+			<!-- Start of food-->
+			<div class="food">
+				<img src="img/bg1.png" alt="img Gal">
+			</div>
+			<!-- End of food-->
+			<!-- Start of  About -->
+			<div class="about" id="about">
+				<div class="about-txt">
+					<h1>ABOUT <b>US</b></h1>
+					<p>A fast food restaurant, also known as a quick service restaurant (QSR) within the industry, is a specific type of restaurant characterized both by its fast food cuisine and by minimal table service. Food served in fast food restaurants typically caters to a "meat-sweet diet" and is offered from a limited menu; is cooked in bulk in advance and kept hot; is finished and packaged to order; and is usually available ready to take away, though seating may be provided.</p>
+					<div class="icon-wrap">
+						<div class="icon-cont">
+							<i class="fa fa-shopping-cart"></i> <br><br><center>Order</center>
+						</div>
+						<div class="icon-cont">
+							<i class="fa fa-money"></i> <br><br><center>Pay</center>
+						</div>
+						<div class="icon-cont">
+							<i class="fa fa-spoon"></i> <br><br><center>Eat</center>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End of  About -->
+			<!-- Start of  Category -->
+			<div class="category">
+					<div class="">
+
+					</div>
+			</div>
+			<!-- End of  Category -->
+
+
+
+		</div>
+		<!-- End of Container -->
+	</body>
+</html>
